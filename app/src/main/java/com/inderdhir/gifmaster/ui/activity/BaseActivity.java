6794e19eb5d7a570015384646d4b68a420732e78
Inder Dhir <inderdhir@hotmail.com>
2016-09-04 13:42:20 -0400

    cleanup

--- app/src/main/java/com/inderdhir/gifmaster/ui/activity/BaseActivity.java
+++ app/src/main/java/com/inderdhir/gifmaster/ui/activity/BaseActivity.java
@@ -1,14 +1,8 @@
 package com.inderdhir.gifmaster.ui.activity;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 
 public abstract class BaseActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(@Nullable final Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
 }
--- app/src/main/java/com/inderdhir/gifmaster/ui/fragment/BaseFragment.java
+++ app/src/main/java/com/inderdhir/gifmaster/ui/fragment/BaseFragment.java
@@ -1,14 +1,8 @@
 package com.inderdhir.gifmaster.ui.fragment;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 
 
 public abstract class BaseFragment extends Fragment {
 
-    @Override
-    public void onCreate(@Nullable final Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
 }
